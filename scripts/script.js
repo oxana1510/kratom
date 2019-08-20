894b626d49073334e9e579d3345b99bb88450206
oxana1510 <oksi666@xaker.ru>
2019-08-20 13:37:12 +0300

    .

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+(function() {
+ 
+  "use strict";
+ 
+  var toggles = document.querySelectorAll(".mobail-menu");
+ 
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+ 
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      $(".menu").toggleClass("responsive");
+    });
+  }
+ 
+})();
+
+
+
+
+//data calendar
+
+
+$( function() {
+  $( "#datepicker" ).datepicker({
+    altField: "#actualDate"
+  });
+} );
+
+
